5c2c5d4ef7670168d0cbfc80b6828d1b6fa7bbb1
Jonas Thies <Jonas.Thies@DLR.de>
2014-07-18 18:58:33 +0000

    moved the eigenvalue subdirectory evp/ into src/evp
    git-svn-id: https://vcs.service.rug.nl/svn/fredwubs/hymls@929 5bcbc087-4b5e-4533-96d0-4097f40ffae9

---
+++ src/evp/AnasaziHymlsAdapter.hpp
@@ -0,0 +1,48 @@
+#ifndef ANASAZI_HYMLS_ADAPTER_HPP
+#define ANASAZI_HYMLS_ADAPTER_HPP
+
+
+#include "AnasaziPreconditionerTraits.hpp"
+#include "HYMLS_Solver.H"
+
+namespace Anasazi {
+
+  //!
+  template <>
+  class PreconditionerTraits<double,Epetra_Vector, HYMLS::Solver> 
+  {
+  public:
+    
+    //! setup HYMLS for using the operator (beta*A-alpha*B)
+    static void SetShift(OP& Op, const ScalarType& beta,
+                                 const ScalarType& alpha)
+    {
+      //TODO
+    }
+    
+    //! set projection vectors V: operator should act as inverse of (I-VV')(beta*A-alpha*B)
+    static void SetProjectionVectors(OP& Op, const Teuchos::RCP<const MV>& Q)
+    {
+      //TODO
+    }
+    
+    //! @name Operator application method.
+    //@{ 
+    
+    //! Application method which performs operation <b>y ~= (I-VV')*(A-sigmaB)\x</b>. 
+    static void ApplyInverse ( const OP& Op, 
+                        const MV& x, 
+                        MV& y )
+    {
+      //TODO
+    }
+    
+    //@}
+    
+  };
+  
+} // end Anasazi namespace
+
+#endif // ANASAZI_PRECONDITIONER_TRAITS_HPP
+
+// end of file AnasaziPreconditionerTraits.hpp
