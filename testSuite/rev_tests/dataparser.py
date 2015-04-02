cdc486a4fad7a0c696aa3971abd3baeef0960ce0
Sven Baars <s.baars@rug.nl>
2015-04-02 09:36:55 +0000

    rev_tests: Output some more graphs
    git-svn-id: https://vcs.service.rug.nl/svn/fredwubs/hymls@1124 5bcbc087-4b5e-4533-96d0-4097f40ffae9

--- testSuite/rev_tests/dataparser.py
+++ testSuite/rev_tests/dataparser.py
@@ -9,6 +9,12 @@ class ParserData:
         self.fvm = {'timing': {}, 'failures': 0}
         self.fvm_testing = {'timing': {}, 'failures': 0}
 
+        self.fvm_parallel = {'timing': {}, 'failures': 0}
+        self.fvm_testing_parallel = {'timing': {}, 'failures': 0}
+
+        self.integration_tests = {'failures': 0}
+        self.integration_tests_testing = {'failures': 0}
+
 class Parser:
     def __init__(self, prefix=None):
         self.prefix = prefix
@@ -21,6 +27,19 @@ class Parser:
             if os.path.isdir(path) and len(i) < 5:
                 self.data[i] = self.parse_dir(path)
                 print self.data[i].fvm
+
+    def parse_integration_test_data(self, data):
+        parsed_data = ParserData()
+        first = True
+        for i in data:
+            if 'TESTS OUT OF' in i:
+                print i
+                if first:
+                    parsed_data.integration_tests['failures'] = int(i[9:11])
+                    first = False
+                else:
+                    parsed_data.integration_tests_testing['failures'] = int(i[9:11])
+        return parsed_data
 
     def parse_fvm_data(self, data):
         timing_started = False
@@ -47,13 +66,28 @@ class Parser:
                 fvm_file = open(fvm_path, 'r')
                 fvm_data = fvm_file.readlines()
                 fvm_file.close()
-                if i.endswith('testing.out'):
-                    data.fvm_testing = self.parse_fvm_data(fvm_data)
+                if '16' in i:
+                    if i.endswith('testing.out'):
+                        data.fvm_testing = self.parse_fvm_data(fvm_data)
+                    else:
+                        data.fvm = self.parse_fvm_data(fvm_data)
                 else:
-                    data.fvm = self.parse_fvm_data(fvm_data)
+                    if i.endswith('testing.out'):
+                        data.fvm_testing_parallel = self.parse_fvm_data(fvm_data)
+                    else:
+                        data.fvm_parallel = self.parse_fvm_data(fvm_data)
+            elif i == 'integration_tests.txt':
+                test_path = os.path.join(path, i)
+                test_file = open(test_path, 'r')
+                test_data = test_file.readlines()
+                test_file.close()
+                parsed_data = self.parse_integration_test_data(test_data)
+                data.integration_tests = parsed_data.integration_tests
+                data.integration_tests_testing = parsed_data.integration_tests_testing
+
         return data
 
-    def plot_timing(self):
+    def plot_timing(self, attr='fvm'):
         figure = pyplot.figure()
         plot = figure.add_subplot(111)
         data_list = ['main: continuation run', 'HYMLS::Solver: ApplyInverse', 'Preconditioner_L1: Compute']
@@ -65,7 +99,7 @@ class Parser:
                 print 'data', data.fvm
                 print rev
                 x.append(int(rev))
-                y.append(float(data.fvm['timing'].get(i, '0')))
+                y.append(float(getattr(data, attr)['timing'].get(i, '0')))
             plot.plot(x, y, linewidth=2)
             plot.hold(True)
         leg = plot.legend(data_list, loc=1)
@@ -73,13 +107,15 @@ class Parser:
         frame.set_alpha(0.5)
         plot.set_xlabel(u'rev', size='large')
         plot.set_ylabel(u'time', size='large')
-        figure.savefig(os.path.join(self.prefix, 'fvm_timing.eps'), bbox_inches='tight')
-        figure.savefig(os.path.join(self.prefix, 'fvm_timing.png'), bbox_inches='tight')
+        figure.savefig(os.path.join(self.prefix, attr+'_timing.eps'), bbox_inches='tight')
+        figure.savefig(os.path.join(self.prefix, attr+'_timing.png'), bbox_inches='tight')
 
 
     def plot_failures(self):
         figure = pyplot.figure()
         plot = figure.add_subplot(111)
+
+        #fvm failures
         x = []
         y = []
         for rev in sorted(self.data.keys(), key=int):
@@ -89,15 +125,43 @@ class Parser:
             y.append(float(data.fvm_testing['failures']))
         plot.plot(x, y, linewidth=2)
         plot.hold(True)
+
+        #integration test failures
+        x = []
+        y = []
+        for rev in sorted(self.data.keys(), key=int):
+            data = self.data[rev]
+            print rev
+            x.append(int(rev))
+            y.append(float(data.integration_tests['failures']))
+        plot.plot(x, y, linewidth=2)
+        plot.hold(True)
+
+        #integration test failures
+        x = []
+        y = []
+        for rev in sorted(self.data.keys(), key=int):
+            data = self.data[rev]
+            print rev
+            x.append(int(rev))
+            y.append(float(data.integration_tests_testing['failures']))
+        plot.plot(x, y, linewidth=2)
+        plot.hold(True)
+
+        data_list = ['FVM', 'integration tests', 'integration tests (testing)']
+        leg = plot.legend(data_list, loc=1)
+        frame = leg.get_frame()
+        frame.set_alpha(0.5)
         plot.set_xlabel(u'rev', size='large')
-        plot.set_ylabel(u'time', size='large')
-        figure.savefig(os.path.join(self.prefix, 'fvm_failures.eps'), bbox_inches='tight')
-        figure.savefig(os.path.join(self.prefix, 'fvm_failures.png'), bbox_inches='tight')
+        plot.set_ylabel(u'failures', size='large')
+        figure.savefig(os.path.join(self.prefix, 'test_failures.eps'), bbox_inches='tight')
+        figure.savefig(os.path.join(self.prefix, 'test_failures.png'), bbox_inches='tight')
 
 def main():
     parser = Parser('./')
     parser.parse()
-    parser.plot_timing()
+    parser.plot_timing('fvm')
+    parser.plot_timing('fvm_parallel')
     parser.plot_failures()
 
 if __name__ == "__main__":
