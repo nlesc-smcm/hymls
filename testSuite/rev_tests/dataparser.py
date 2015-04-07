a509c30cf0252aec40c279f121fdf70402986ae4
Sven Baars <s.baars@rug.nl>
2015-04-07 07:19:01 +0000

    rev_tests: Add an iterations plot
    git-svn-id: https://vcs.service.rug.nl/svn/fredwubs/hymls@1133 5bcbc087-4b5e-4533-96d0-4097f40ffae9

--- testSuite/rev_tests/dataparser.py
+++ testSuite/rev_tests/dataparser.py
@@ -7,11 +7,11 @@ import re
 
 class ParserData:
     def __init__(self):
-        self.fvm = {'timing': {}, 'failures': 0}
-        self.fvm_testing = {'timing': {}, 'failures': 0}
+        self.fvm = {'timing': {}, 'failures': 0, 'iterations': 0}
+        self.fvm_testing = {'timing': {}, 'failures': 0, 'iterations': 0}
 
-        self.fvm_parallel = {'timing': {}, 'failures': 0}
-        self.fvm_testing_parallel = {'timing': {}, 'failures': 0}
+        self.fvm_parallel = {'timing': {}, 'failures': 0, 'iterations': 0}
+        self.fvm_testing_parallel = {'timing': {}, 'failures': 0, 'iterations': 0}
 
         self.integration_tests = {'failures': 0}
         self.integration_tests_testing = {'failures': 0}
@@ -24,7 +24,7 @@ class Parser:
     def parse(self):
         for i in os.listdir(self.prefix):
             path = os.path.join(self.prefix, i)
-            if os.path.isdir(path) and len(i) < 5:
+            if os.path.isdir(path) and len(i) < 5 and i.isdigit():
                 print i
                 self.data[i] = self.parse_dir(path)
                 #~ print self.data[i].fvm
@@ -44,7 +44,7 @@ class Parser:
 
     def parse_fvm_data(self, data):
         timing_started = False
-        parsed_data = {'timing': {}, 'failures': 0}
+        parsed_data = {'timing': {}, 'failures': 0, 'iterations': 0}
         for i in data:
             if 'TIMING RESULTS' in i:
                 timing_started = True
@@ -57,6 +57,8 @@ class Parser:
                     parsed_data['timing'][name] = d[3].strip()
             if 'FAILED!' in i:
                 parsed_data['failures'] += 1
+            if i.startswith('+ Number of iterations:'):
+                parsed_data['iterations'] = int(i[23:])
         return parsed_data
 
     def parse_dir(self, path):
@@ -89,10 +91,43 @@ class Parser:
 
         return data
 
+    def plot_iterations(self):
+        figure = pyplot.figure()
+        plot = figure.add_subplot(111)
+        prev_its = 0
+        for attr in ('fvm', 'fvm_parallel'):
+            x = []
+            y = []
+            for rev in sorted(self.data.keys(), key=int):
+                data = self.data[rev]
+                its = int(getattr(data, attr)['iterations'])
+                if its > 0:
+                    x.append(int(rev))
+                    y.append(its)
+                    prev_its = its
+                else:
+                    plot.annotate('x', xy=(int(rev), prev_its), xytext=(0,0), textcoords='offset points')
+            plot.plot(x, y, linewidth=2)
+            yprev = 0
+            for (i,j) in zip(x,y):
+                if abs(yprev - j) > 0.05 * j:
+                    plot.annotate(str(i), xy=(i,j), xytext=(-10, 5), textcoords='offset points')
+                yprev = j
+            plot.hold(True)
+        leg = plot.legend(['FVM', 'FVM (parallel)'], loc=1)
+        frame = leg.get_frame()
+        frame.set_alpha(0.5)
+        plot.set_xlabel(u'rev', size='large')
+        plot.set_ylabel(u'time', size='large')
+        figure.savefig(os.path.join(self.prefix, 'fvm_iterations.eps'), bbox_inches='tight')
+        figure.savefig(os.path.join(self.prefix, 'fvm_iterations.png'), bbox_inches='tight')
+
+
     def plot_timing(self, attr='fvm'):
         figure = pyplot.figure()
         plot = figure.add_subplot(111)
         data_list = ['main: continuation run', 'HYMLS::Solver: ApplyInverse', 'Preconditioner (level 1): Compute']
+        prev_time = 0
         for i in data_list:
             x = []
             y = []
@@ -104,6 +139,9 @@ class Parser:
                 if time > 0:
                     x.append(int(rev))
                     y.append(time)
+                    prev_time = time
+                else:
+                    plot.annotate('x', xy=(int(rev), prev_time), xytext=(0,0), textcoords='offset points')
             plot.plot(x, y, linewidth=2)
             yprev = 0
             for (i,j) in zip(x,y):
@@ -156,6 +194,7 @@ def main():
     parser.parse()
     parser.plot_timing('fvm')
     parser.plot_timing('fvm_parallel')
+    parser.plot_iterations()
     parser.plot_failures()
 
 if __name__ == "__main__":
