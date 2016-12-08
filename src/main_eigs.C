d6591470a0fc7241a575730de91b90411ada21d2
Jonas Thies <Jonas.Thies@DLR.de>
2016-12-08 13:45:47 +0100

    also check perio in main_eigs, and adjusted sought EVs in Turing XML files to LR

--- src/main_eigs.C
+++ src/main_eigs.C
@@ -174,6 +174,35 @@ bool status=true;
     int ny=probl_params.get("ny",nx);
     int nz=probl_params.get("nz",dim>2?nx:1);
     int dof=probl_params.get("Degrees of Freedom",2);   
+
+    GaleriExt::PERIO_Flag perio=probl_params.get("Periodicity",GaleriExt::NO_PERIO);
+    if (probl_params.isParameter("x-periodic"))
+    {
+      if (probl_params.get("x-periodic",false)==true)
+      {
+        (uint32_t&)perio |= (uint32_t)GaleriExt::X_PERIO;
+      }
+      if (read_problem) probl_params.remove("x-periodic");
+    }
+    if (probl_params.isParameter("y-periodic"))
+    {
+      if (probl_params.get("y-periodic",false)==true)
+      {
+        (uint32_t&)perio |= (uint32_t)GaleriExt::Y_PERIO;
+      }
+      if (read_problem) probl_params.remove("y-periodic");
+    }
+    if (probl_params.isParameter("z-periodic"))
+    {
+      if (probl_params.get("z-periodic",false)==true)
+      {
+        (uint32_t&)perio |= (uint32_t)GaleriExt::Z_PERIO;
+      }
+      if (read_problem) probl_params.remove("z-periodic");
+    }
+    
+    if (read_problem) probl_params.set("Periodicity",perio);
+
  
     std::string eqn=probl_params_cpy.get("Equations","Laplace");
 
