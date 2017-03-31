fec11c62d2b4a0f47da5e2c012b515fa24836e21
Sven Baars <s.baars@rug.nl>
2017-03-31 10:35:45 +0200

    Add a mex interface
    Note that after compiling, I need to run Matlab like this:
    
    LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6 matlab
    
    because it ships with an older libstdc++.
    
    I do not do enough checks, so sometimes it still segfaults when it
    is used in a wrong way.

---
+++ matlab/HYMLS_apply.C
@@ -0,0 +1,36 @@
+#include "mex.h"
+
+#include "HYMLS_Preconditioner.H"
+
+#include "Teuchos_RCP.hpp"
+#include "Teuchos_ParameterList.hpp"
+#include "Teuchos_XMLParameterListHelpers.hpp"
+
+#include "Epetra_Map.h"
+#include "Epetra_CrsMatrix.h"
+
+void mexFunction(int nlhs, mxArray *plhs[],
+                 int nrhs, const mxArray *prhs[])
+{
+    if (nrhs != 2)
+        mexErrMsgIdAndTxt("HYMLS_init:nrhs", "Two input arguments required.");
+    if (nlhs != 1)
+        mexErrMsgIdAndTxt("HYMLS_init:nlhs", "One output argument required.");
+
+    // Get back the HYMLS object
+    if (mxGetNumberOfElements(prhs[0]) != 1 || mxGetClassID(prhs[0]) != mxUINT64_CLASS)
+      mexErrMsgIdAndTxt("HYMLS_init:prhs", "Input must be a real uint64 scalar.");
+    HYMLS::Preconditioner *prec = reinterpret_cast<HYMLS::Preconditioner *>(*((uint64_t *)mxGetData(prhs[0])));
+
+    double *x = mxGetPr(prhs[1]);
+    int m = mxGetM(prhs[1]);
+    int n = mxGetN(prhs[1]);
+
+    plhs[0] = mxCreateDoubleMatrix((mwSize)m, (mwSize)n, mxREAL);
+    double *y = mxGetPr(plhs[0]);
+
+    Epetra_MultiVector xmv(View, prec->OperatorDomainMap(), x, m ,n);
+    Epetra_MultiVector ymv(View, prec->OperatorRangeMap(), y, m, n);
+
+    prec->ApplyInverse(xmv, ymv);
+}
