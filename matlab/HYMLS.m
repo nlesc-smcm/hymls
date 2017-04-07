6d476cd814adac7215f8e7464daa1a4efed7fdb2
Sven Baars <s.baars@rug.nl>
2017-04-07 10:01:44 +0200

    Add a set_border method to the mex interface

--- matlab/HYMLS.m
+++ matlab/HYMLS.m
@@ -17,6 +17,16 @@ classdef HYMLS < handle
             y = HYMLS_apply(h.preconditioner, x);
         end
 
+        function set_border(h, v, w)
+            if nargin == 2
+                 HYMLS_set_border(h.preconditioner, v);
+            elseif nargin == 3
+                HYMLS_set_border(h.preconditioner, v, w);
+            else
+                error('One input argument required');
+            end
+        end
+
         function delete(h)
             if ~isempty(h.preconditioner)
                 HYMLS_free(h.preconditioner);
--- matlab/test/test_stokes0.m
+++ matlab/test/test_stokes0.m
@@ -37,11 +37,22 @@ classdef test_stokes0 < matlab.unittest.TestCase
             testCase.verifyLessThan(norm(x-x3), 1e-9);
         end
 
-        function testStokes0WithClass(testCase)
+        function testStokes0WithBorder(testCase)
             A = mmread([fileparts(mfilename('fullpath')) ...
                         '/../../testSuite/data/DrivenCavity/32x32/Re0/jac.mtx']);
-            P = HYMLS(A, [fileparts(mfilename('fullpath')) ...
-                               '/../../testSuite/integration_tests/stokes0.xml']);
+            xmlfile = tempname;
+            xmltext = fileread([fileparts(mfilename('fullpath')) ...
+                                '/../../testSuite/integration_tests/stokes0.xml']);
+            xmltext = strrep(...
+                xmltext,...
+                '    <Parameter name="Number of Levels" type="int" value="1"/>',...
+                ['    <Parameter name="Number of Levels" type="int" value="1"/>\n'...
+                 '    <Parameter name="Fix Pressure Level" type="bool" value="0"/>']);
+            fileID = fopen(xmlfile, 'w');
+            fprintf(fileID, xmltext);
+            fclose(fileID);
+            fprintf(xmltext)
+            P = HYMLS(A, xmlfile);
             b = mmread([fileparts(mfilename('fullpath')) ...
                         '/../../testSuite/data/DrivenCavity/32x32/Re0/rhs.mtx']);
             x = P.apply(b);
@@ -60,12 +71,12 @@ classdef test_stokes0 < matlab.unittest.TestCase
             idxv = 1:n;
             idxv(idxp) = [];
             testCase.verifyLessThan(norm(x(idxv)-x2(idxv)), 1e-9);
+            testCase.verifyGreaterThan(norm(x(idxp)-x2(idxp)), 1e-9);
 
-            % Use Dirichlet boundary condition
-            A(3,:) = sparse(1, n);
-            A(3,3) = 1;
-            x3 = A \ b;
-            testCase.verifyLessThan(norm(x-x3), 1e-9);
+            P.set_border(v);
+            x3 = P.apply(b);
+
+            testCase.verifyLessThan(norm(x2-x3), 1e-9);
         end
     end
 end
