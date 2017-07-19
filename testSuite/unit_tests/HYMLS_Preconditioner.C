9fcea39ab3048662d91c32d3a80e75035c6e92a9
Sven Baars <s.baars@rug.nl>
2017-07-19 09:19:12 +0200

    Add a helper function to the Preconditioner tests

--- testSuite/unit_tests/HYMLS_Preconditioner.C
+++ testSuite/unit_tests/HYMLS_Preconditioner.C
@@ -60,10 +60,8 @@ public:
     }
   };
 
-TEUCHOS_UNIT_TEST(Preconditioner, Blocks)
+TestablePreconditioner createPreconditioner(Epetra_Comm &Comm)
   {
-  Epetra_MpiComm Comm(MPI_COMM_WORLD);
-
   Teuchos::RCP<Teuchos::ParameterList> params = Teuchos::rcp(new Teuchos::ParameterList());
   Teuchos::ParameterList &problemList = params->sublist("Problem");
   problemList.set("Degrees of Freedom", 4);
@@ -103,10 +101,18 @@ TEUCHOS_UNIT_TEST(Preconditioner, Blocks)
   CHECK_ZERO(A->FillComplete());
 
   TestablePreconditioner prec(A, params);
+  return prec;
+  }
+
+TEUCHOS_UNIT_TEST(Preconditioner, Blocks)
+  {
+  Epetra_MpiComm Comm(MPI_COMM_WORLD);
+  TestablePreconditioner prec = createPreconditioner(Comm);
   prec.Initialize();
   prec.Compute();
 
-  Teuchos::RCP<Epetra_CrsMatrix> B = Teuchos::rcp(new Epetra_CrsMatrix(*A));
+  Teuchos::RCP<Epetra_CrsMatrix> B = Teuchos::rcp(new Epetra_CrsMatrix(
+      dynamic_cast<Epetra_CrsMatrix const&>(prec.Matrix())));
   prec.SetMatrix(B);
   prec.Initialize();
   prec.Compute();
@@ -119,33 +125,7 @@ TEUCHOS_UNIT_TEST(Preconditioner, SerialComm)
   {
   // Check if HYMLS can work without MPI
   Epetra_SerialComm Comm;
-
-  Teuchos::RCP<Teuchos::ParameterList> params = Teuchos::rcp(new Teuchos::ParameterList());
-  Teuchos::ParameterList &problemList = params->sublist("Problem");
-  problemList.set("Degrees of Freedom", 4);
-  problemList.set("Dimension", 3);
-  problemList.set("nx", 8);
-  problemList.set("ny", 4);
-  problemList.set("nz", 4);
-  Teuchos::ParameterList &precList = params->sublist("Preconditioner");
-  precList.set("Separator Length", 4);
-  precList.set("Number of Levels", 1);
-
-  int n = problemList.get<int>("Degrees of Freedom") * problemList.get<int>("nx") *
-    problemList.get<int>("ny") * problemList.get<int>("nz");
-  Epetra_Map map(n, 0, Comm);
-
-  Teuchos::RCP<Epetra_CrsMatrix> A = Teuchos::rcp(new Epetra_CrsMatrix(Copy, map, 2));
-  Epetra_Util util;
-  for (int i = 0; i < n; i++) {
-    // int A_idx = util.RandomInt() % n;
-    double A_val = util.RandomDouble();
-    // A->InsertGlobalValues(i, 1, &A_val, &A_idx);
-    A->InsertGlobalValues(i, 1, &A_val, &i);
-  }
-  A->FillComplete();
-
-  TestablePreconditioner prec(A, params);
+  TestablePreconditioner prec = createPreconditioner(Comm);
   prec.Initialize();
   prec.Compute();
   }
@@ -153,49 +133,11 @@ TEUCHOS_UNIT_TEST(Preconditioner, SerialComm)
 TEUCHOS_UNIT_TEST(Preconditioner, setBorder)
   {
   Epetra_MpiComm Comm(MPI_COMM_WORLD);
-
-  Teuchos::RCP<Teuchos::ParameterList> params = Teuchos::rcp(new Teuchos::ParameterList());
-  Teuchos::ParameterList &problemList = params->sublist("Problem");
-  problemList.set("Degrees of Freedom", 4);
-  problemList.set("Dimension", 3);
-  problemList.set("nx", 8);
-  problemList.set("ny", 4);
-  problemList.set("nz", 4);
-  Teuchos::ParameterList &precList = params->sublist("Preconditioner");
-  precList.set("Separator Length", 4);
-  precList.set("Number of Levels", 1);
-
-  int n = problemList.get<int>("Degrees of Freedom") * problemList.get<int>("nx") *
-    problemList.get<int>("ny") * problemList.get<int>("nz");
-  Epetra_Map map(n, 0, Comm);
-
-  HYMLS::CartesianPartitioner part(Teuchos::rcp(&map, false),
-    problemList.get<int>("nx"), problemList.get<int>("ny"), problemList.get<int>("nz"),
-    problemList.get<int>("Degrees of Freedom"));
-  part.Partition(2, true);
-  map = *part.GetMap();
-
-  Teuchos::RCP<Epetra_CrsMatrix> A = Teuchos::rcp(new Epetra_CrsMatrix(Copy, map, 2));
-
-  Epetra_Util util;
-  for (int i = 0; i < n; i++) {
-    // int A_idx = util.RandomInt() % n;
-    // double A_val = -std::abs(util.RandomDouble());
-    double A_val2 = std::abs(util.RandomDouble());
-
-    // Check if we own the index
-    if (A->LRID(i) == -1)
-      continue;
-
-    // CHECK_ZERO(A->InsertGlobalValues(i, 1, &A_val, &A_idx));
-    CHECK_ZERO(A->InsertGlobalValues(i, 1, &A_val2, &i));
-  }
-  CHECK_ZERO(A->FillComplete());
-
-  TestablePreconditioner prec(A, params);
+  TestablePreconditioner prec = createPreconditioner(Comm);
   prec.Initialize();
   prec.Compute();
 
+  Epetra_Map const &map = prec.OperatorDomainMap();
   Teuchos::RCP<Epetra_MultiVector> V = Teuchos::rcp(new Epetra_MultiVector(map, 1));
   V->Random();
 
@@ -214,46 +156,7 @@ TEUCHOS_UNIT_TEST(Preconditioner, setBorder)
 TEUCHOS_UNIT_TEST(Preconditioner, setBorderNull)
   {
   Epetra_MpiComm Comm(MPI_COMM_WORLD);
-
-  Teuchos::RCP<Teuchos::ParameterList> params = Teuchos::rcp(new Teuchos::ParameterList());
-  Teuchos::ParameterList &problemList = params->sublist("Problem");
-  problemList.set("Degrees of Freedom", 4);
-  problemList.set("Dimension", 3);
-  problemList.set("nx", 8);
-  problemList.set("ny", 4);
-  problemList.set("nz", 4);
-  Teuchos::ParameterList &precList = params->sublist("Preconditioner");
-  precList.set("Separator Length", 4);
-  precList.set("Number of Levels", 1);
-
-  int n = problemList.get<int>("Degrees of Freedom") * problemList.get<int>("nx") *
-    problemList.get<int>("ny") * problemList.get<int>("nz");
-  Epetra_Map map(n, 0, Comm);
-
-  HYMLS::CartesianPartitioner part(Teuchos::rcp(&map, false),
-    problemList.get<int>("nx"), problemList.get<int>("ny"), problemList.get<int>("nz"),
-    problemList.get<int>("Degrees of Freedom"));
-  part.Partition(2, true);
-  map = *part.GetMap();
-
-  Teuchos::RCP<Epetra_CrsMatrix> A = Teuchos::rcp(new Epetra_CrsMatrix(Copy, map, 2));
-
-  Epetra_Util util;
-  for (int i = 0; i < n; i++) {
-    // int A_idx = util.RandomInt() % n;
-    // double A_val = -std::abs(util.RandomDouble());
-    double A_val2 = std::abs(util.RandomDouble());
-
-    // Check if we own the index
-    if (A->LRID(i) == -1)
-      continue;
-
-    // CHECK_ZERO(A->InsertGlobalValues(i, 1, &A_val, &A_idx));
-    CHECK_ZERO(A->InsertGlobalValues(i, 1, &A_val2, &i));
-  }
-  CHECK_ZERO(A->FillComplete());
-
-  TestablePreconditioner prec(A, params);
+  TestablePreconditioner prec = createPreconditioner(Comm);
   prec.Initialize();
   prec.Compute();
 
@@ -262,6 +165,7 @@ TEUCHOS_UNIT_TEST(Preconditioner, setBorderNull)
   TEST_EQUALITY(prec.V(), Teuchos::null);
 
   // Set the border to something else
+  Epetra_Map const &map = prec.OperatorDomainMap();
   Teuchos::RCP<Epetra_MultiVector> V = Teuchos::rcp(new Epetra_MultiVector(map, 1));
   V->Random();
 
