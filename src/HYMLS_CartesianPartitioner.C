79d0219d6566de563ea6f3a91ec4431110be5ded
Sven Baars <s.baars@rug.nl>
2018-05-09 10:52:20 +0200

    Move Repartitioning from CartesianPartitioner to BasePartitioner

--- src/HYMLS_CartesianPartitioner.C
+++ src/HYMLS_CartesianPartitioner.C
@@ -6,8 +6,6 @@
 #include "Epetra_IntVector.h"
 #include "Epetra_Import.h"
 
-#include "Epetra_Distributor.h"
-
 using Teuchos::toString;
 
 namespace HYMLS {
@@ -189,84 +187,7 @@ int CartesianPartitioner::Partition(bool repart)
   if (repart)
     {
     Tools::Out("repartition for "+s4+" procs");
-    HYMLS_PROF3(label_,"repartition map");
-
-    Teuchos::RCP<Epetra_Distributor> Distor =
-      Teuchos::rcp(comm_->CreateDistributor());
-
-    int myPID = comm_->MyPID();
-
-    // check how many of the owned GIDs in the map need to be
-    // moved to someone else
-    int numSends = 0;
-    for (int lid = 0; lid < baseMap_->NumMyElements(); lid++)
-    {
-        hymls_gidx gid = baseMap_->GID64(lid);
-        if (PID(gid) != myPID)
-          numSends++;
-    }
-
-    int numLocal = baseMap_->NumMyElements() - numSends;
-
-    // determine which GIDs we have to move, and where they will go
-    hymls_gidx *sendGIDs = new hymls_gidx[numSends];
-    int *sendPIDs = new int[numSends];
-
-    int pos = 0;
-    for (int i = 0; i < baseMap_->NumMyElements(); i++)
-      {
-      hymls_gidx gid = baseMap_->GID64(i);
-      int pid = PID(gid); // global partition ID
-      if (pid != myPID)
-        {
-        if (pos >= numSends)
-          {
-          Tools::Error("Sanity check failed with gid = " + Teuchos::toString(gid) +
-            ", pos = " + Teuchos::toString(pos) + ", numSends = " +
-            Teuchos::toString(numSends) + ".", __FILE__, __LINE__);
-          }
-        sendGIDs[pos] = gid;
-        sendPIDs[pos++] = pid;
-        }
-      }
-
-    int numRecvs;
-    CHECK_ZERO(Distor->CreateFromSends(numSends, sendPIDs, true, numRecvs));
- 
-    char* sbuf = reinterpret_cast<char*>(sendGIDs);
-    int numRecvChars = static_cast<int>(numRecvs * sizeof(hymls_gidx));
-    char* rbuf = new char[numRecvChars];
-
-    CHECK_ZERO(Distor->Do(sbuf, sizeof(hymls_gidx), numRecvChars, rbuf));
-
-    hymls_gidx *recvGIDs = reinterpret_cast<hymls_gidx*>(rbuf);
-    if (static_cast<int>(numRecvs * sizeof(hymls_gidx)) != numRecvChars)
-      {
-      Tools::Error("sanity check failed", __FILE__, __LINE__);
-      }
-
-    int NumMyElements = numLocal + numRecvs;
-    hymls_gidx *MyGlobalElements = new hymls_gidx[NumMyElements];
-    pos = 0;
-    for (int i = 0; i < baseMap_->NumMyElements(); i++)
-      {
-      hymls_gidx gid = baseMap_->GID64(i);
-      if (PID(gid) == myPID)
-        MyGlobalElements[pos++] = gid;
-      }
-
-    for (int i = 0; i < numRecvs; i++)
-      MyGlobalElements[pos+i] = recvGIDs[i];
-
-    std::sort(MyGlobalElements, MyGlobalElements + NumMyElements);
-
-    cartesianMap_ = Teuchos::rcp(new Epetra_Map(
-        -1, NumMyElements, MyGlobalElements, (hymls_gidx)baseMap_->IndexBase64(), *comm_));
-
-    delete [] sendPIDs;
-    delete [] sendGIDs;
-    delete [] rbuf;
-    delete [] MyGlobalElements;
+    cartesianMap_ = RepartitionMap(baseMap_);
     }
 
 #ifdef HYMLS_TESTING
