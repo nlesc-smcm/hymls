89865ad0f7fd2cfeadb57345a16722f252d74334
Sven Baars <s.baars@rug.nl>
2020-03-20 18:02:11 +0100

    vsm: Allow reindexing to group velocities together

--- python/vsm.py
+++ python/vsm.py
@@ -8,25 +8,21 @@ def wrtbcsr(beg, jco, co, f):
     n = numpy.int32(len(beg) - 1)
 
     bc = numpy.int32(n.nbytes)
-    print(bc, bc.tobytes())
     os.write(f, bc.tobytes())
     os.write(f, n.tobytes())
     os.write(f, bc.tobytes())
 
     bc = numpy.int32(beg.nbytes)
-    print(bc, bc.tobytes())
     os.write(f, bc.tobytes())
     os.write(f, beg.tobytes())
     os.write(f, bc.tobytes())
 
     bc = numpy.int32(jco.nbytes)
-    print(bc, bc.tobytes())
     os.write(f, bc.tobytes())
     os.write(f, jco.tobytes())
     os.write(f, bc.tobytes())
 
     bc = numpy.int32(co.nbytes)
-    print(bc, bc.tobytes())
     os.write(f, bc.tobytes())
     os.write(f, co.tobytes())
     os.write(f, bc.tobytes())
@@ -37,7 +33,7 @@ def main():
 
     n = -1
     if len(sys.argv) > 2:
-        n = int(sys.argv[2])
+        n = eval(sys.argv[2])
 
     dof = -1
     if len(sys.argv) > 3:
@@ -61,26 +57,44 @@ def main():
                 first = False
                 continue
 
-            row = int(spline[0]) - 1
+            row = int(spline[0])
             col = int(spline[1])
             val = float(spline[2])
             while len(rows) <= row:
                 rows.append(list())
             rows[row].append((col, val))
 
-        n = max(n, len(rows))
-        beg = numpy.ndarray(n + 1, numpy.int32)
-        jco = numpy.ndarray(nnz, numpy.int32)
-        co = numpy.ndarray(nnz, numpy.float)
+    # Reindex to group variables together
+    if dof > 0:
+        index = 1
+        index_map = [0] * (n+1)
+        for i in range(1, dof+1):
+            for j in range(i, n+1, dof):
+                index_map[j] = index
+                index += 1
 
-        idx = 0
-        for i in range(n):
-            beg[i] = idx + 1
-            for j, v in rows[i]:
-                jco[idx] = j
-                co[idx] = v
-                idx += 1
-        beg[n] = idx + 1
+        reindexed_rows = list()
+        while len(reindexed_rows) <= n:
+            reindexed_rows.append(list())
+
+        for i, row in enumerate(rows):
+            for col, val in row:
+                reindexed_rows[index_map[i]].append((index_map[col], val))
+        rows = reindexed_rows
+
+    n = max(n, len(rows)-1)
+    beg = numpy.ndarray(n + 1, numpy.int32)
+    jco = numpy.ndarray(nnz, numpy.int32)
+    co = numpy.ndarray(nnz, numpy.float)
+
+    idx = 0
+    for i in range(n):
+        beg[i] = idx + 1
+        for j, v in rows[i+1]:
+            jco[idx] = j
+            co[idx] = v
+            idx += 1
+    beg[n] = idx + 1
 
     f, fname = tempfile.mkstemp()
     wrtbcsr(beg, jco, co, f)
