c983a76318385127f6020626b15c035238cec353
Sven Baars <s.baars@rug.nl>
2020-03-20 13:13:01 +0100

    Throw an error if there is a separator group with length 0, even outside of testing mode

--- src/HYMLS_SchurPreconditioner.cpp
+++ src/HYMLS_SchurPreconditioner.cpp
@@ -654,20 +654,6 @@ int SchurPreconditioner::InitializeBlocks()
     numBlocks += sepObject->NumSeparatorGroups(sd);
     }
 
-#ifdef HYMLS_TESTING
-  for (int sd = 0; sd < sepObject->NumMySubdomains(); sd++)
-    {
-    for (SeparatorGroup const &group: sepObject->GetSeparatorGroups(sd))
-      {
-      if (group.length() == 0)
-        {
-        HYMLS::Tools::Warning("there is an empty separator, which is probably dangerous",
-          __FILE__,__LINE__);
-        }
-      }
-    }
-#endif
-
   // create an array of solvers for all the diagonal blocks
   blockSolver_.resize(numBlocks);
   double nnz = 0.0;
@@ -676,6 +662,9 @@ int SchurPreconditioner::InitializeBlocks()
     {
     for (SeparatorGroup const &group: sepObject->GetSeparatorGroups(sd))
       {
+      if (group.length() == 0)
+        HYMLS::Tools::Error("there is an empty separator, which is probably dangerous", __FILE__, __LINE__);
+
       // in the spawned sepObject, each local separator is a group of a subdomain.
       // -1 because we remove one Vsum node from each block
       int numRows = std::max(group.length() - 1, 0);
